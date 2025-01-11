4520a1d1fb7bea5e2835c8498cb5bf2008c6a827
Nolij <xdmatthewbx@mixhosting.xyz>
2025-01-11 11:16:19 -0500

    cleanup

--- src/main/java/dev/nolij/libnolij/util/UnsafeUtil.java
+++ src/main/java/dev/nolij/libnolij/util/UnsafeUtil.java
@@ -22,11 +22,11 @@ public final class UnsafeUtil {
             
             final Field internalLookupField = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
 			//noinspection deprecation
-			var fieldOffset = UNSAFE.staticFieldOffset(internalLookupField);
+			final long fieldOffset = UNSAFE.staticFieldOffset(internalLookupField);
 			//noinspection deprecation
-			var fieldBase = UNSAFE.staticFieldBase(internalLookupField);
-            INTERNAL_LOOKUP = (MethodHandles.Lookup) UNSAFE.getObject(fieldBase, fieldOffset);
+			final Object fieldBase = UNSAFE.staticFieldBase(internalLookupField);
             
+            INTERNAL_LOOKUP = (MethodHandles.Lookup) UNSAFE.getObject(fieldBase, fieldOffset);
             UNSAFE_REFRACTION = new Refraction(INTERNAL_LOOKUP, Refraction.class.getClassLoader());
         } catch (Throwable e) {
             throw new AssertionError(e);
